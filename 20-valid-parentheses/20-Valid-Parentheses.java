cbd08887c4938341c26c9c81660450c22b67f009
Vikrant Verma <vikrant377.vr@gmail.com>
2026-07-09 21:00:04 +0530

    [20] [Time Beats: 87.15%] [Memory Beats: 10.41%] - LeetPush

--- 20-valid-parentheses/20-Valid-Parentheses.java
+++ 20-valid-parentheses/20-Valid-Parentheses.java
@@ -1,16 +1,32 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character>st=new Stack<>();
+        Deque<Character>st=new ArrayDeque<>();
         for(char ch:s.toCharArray()){
-            if(ch=='('||ch=='['||ch=='{'){
-                st.push(ch);
-            }else{
-                char tp=st.peek();
-                if(ch==')' && tp=='(') st.pop();
-                else if(ch==']' && tp=='[') st.pop();
-                else if(ch=='}' && tp=='{') st.pop();
+            if(ch=='(')st.push(')');
+            else if(ch=='{')st.push('}');
+            else if(ch=='[')st.push(']');
+            else{
+                if(st.isEmpty()||st.pop()!=ch){
+                    return false;
+                }
             }
         }
         return st.isEmpty();
+
+
+        // Deque<Character> st = new ArrayDeque<>();
+        // for(char ch:s.toCharArray()){
+        //     if(ch=='('||ch=='['||ch=='{'){
+        //         st.push(ch);
+        //     }else{
+        //         if(st.isEmpty()) return false;
+        //         char tp=st.peek();
+        //         if(ch==')' && tp=='(') st.pop();
+        //         else if(ch==']' && tp=='[') st.pop();
+        //         else if(ch=='}' && tp=='{') st.pop();
+        //         else return false;
+        //     }
+        // }
+        // return st.isEmpty();
     }
 }
