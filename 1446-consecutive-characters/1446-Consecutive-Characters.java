d95b30a0688855766b53614a96452d5542e0ebc3
Vikrant Verma <vikrant377.vr@gmail.com>
2026-07-16 13:54:05 +0530

    [1446] [Time Beats: 100.00%] [Memory Beats: 98.13%] - LeetPush

---
+++ 1446-consecutive-characters/1446-Consecutive-Characters.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxPower(String s) {
+        int count=1;
+        int max=1;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                count++;
+            }else{
+                count=1;
+            }
+            max=Math.max(count,max);
+        }
+        return max;
+    }
+}
